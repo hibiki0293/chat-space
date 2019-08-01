2ff80ae61c59c900903e4752185db4df82328b40
hibiki0293 <hibiki893@i.softbank.jp>
2019-08-01 13:13:46 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -22,7 +22,6 @@ $(function() {
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
-    console.log(url);
     $.ajax({
       url: url,
       type: "POST",
@@ -34,9 +33,9 @@ $(function() {
   .done(function(data){
     var html = buildHTML(data);
     $('.rightside__comments').append(html)
-    $('.rightside__form_input').val('')
+    $('#new_message')[0].reset()
     $('.rightside__comments').animate({scrollTop: $('.rightside__comments')[0].scrollHeight})
-    })
+  })
   .fail(function(){
     alert('error');
     })
