1e13e88ccd3f18811c965818ec5842a36db9c503
hibiki0293 <hibiki893@i.softbank.jp>
2019-08-04 15:50:47 +0900

    auto

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(function() {
   function buildHTML(message){
     var Addimage = (message.image.url !== null)? `<img src="${message.image.url}" class="lower-message__image">`:''
     var html = `
-      <div class="rightside__comments_user">
+      <div class="rightside__comments_user" data-id = "${message.id}">
       <div class="rightside__comments_user-name">
         ${message.user.name} 
         </div>
@@ -47,4 +47,37 @@ $(function() {
 
   })
 
+
+  var reloadMessages = function() {
+    console.log("aaaa")
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id =  $(".rightside__comments_user:last").data('id');
+    group_id = $(".rightside__group").data('id');
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: `/groups/${group_id}/api/messages`,
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log(messages);
+      var insertHTML = '';
+      messages.forEach(function(message){
+        insertHTML = buildHTML(message)
+        $('.rightside__comments').append(insertHTML)
+        $('.rightside__comments').animate({scrollTop: $('.rightside__comments')[0].scrollHeight}, "fasts")
+      });
+
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  group_id = $(".rightside__group").data('id');
+  if(location.pathname == `/groups/${group_id}/messages`){
+  setInterval(reloadMessages, 5000);
+  }
 });
